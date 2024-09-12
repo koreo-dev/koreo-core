c5b2231ccebdc30d7cadf2c87c464af4ea1cb0e5
Robert Kluin <robert.kluin@realkinetic.com>
2024-09-11 22:04:17 -0600

    Start refactoring function reconcile flow into functions

--- src/koreo/function/reconcile.py
+++ src/koreo/function/reconcile.py
@@ -1,3 +1,5 @@
+from typing import Any, NamedTuple
+
 import logging
 
 import copy
@@ -7,10 +9,21 @@ import kr8s
 import jsonpath_ng
 
 import celpy
+from celpy import celtypes
 
-from ..result import DepSkip, Ok, Outcome, PermFail, Retry, Skip, combine, is_ok
+from ..result import (
+    DepSkip,
+    Ok,
+    Outcome,
+    PermFail,
+    Retry,
+    Skip,
+    combine,
+    is_error,
+    is_ok,
+)
 
-from .structure import Function
+from .structure import Function, ManagedResource
 
 
 async def reconcile_function(
@@ -20,195 +33,69 @@ async def reconcile_function(
     trigger_spec: dict,
     inputs: dict,
 ):
-    converted_inputs = celpy.json_to_cel(inputs)
-    trigger_resource = celpy.json_to_cel(
-        {"metadata": trigger_metadata, "spec": trigger_spec}
+    base_inputs = {
+        "inputs": celpy.json_to_cel(inputs),
+        "parent": celpy.json_to_cel(
+            {"metadata": trigger_metadata, "spec": trigger_spec}
+        ),
+    }
+
+    validation_outcome = _run_checks(
+        checks=function.input_validators,
+        inputs=base_inputs,
     )
+    if not is_ok(validation_outcome):
+        return validation_outcome
 
-    if function.input_validators:
-        validation_results = json.loads(
-            json.dumps(
-                function.input_validators.evaluate(
-                    {
-                        "inputs": converted_inputs,
-                        "parent": trigger_resource,
-                    }
-                )
-            )
-        )
-        validation_outcome = _predicate_to_koreo_result(validation_results)
-
-        if not is_ok(validation_outcome):
-            return validation_outcome
-
-    managed_resource = copy.deepcopy(function.template)
-
-    if function.materializers.base:
-        base_overlay = json.loads(
-            json.dumps(
-                function.materializers.base.evaluate(
-                    {
-                        "inputs": converted_inputs,
-                        "parent": trigger_resource,
-                        "template": celpy.json_to_cel(managed_resource),
-                    }
-                )
-            )
-        )
-
-        if base_overlay:
-            for field_path, value in base_overlay.items():
-                field_expr = jsonpath_ng.parse(field_path)
-                field_expr.update_or_create(managed_resource, value)
+    # Create base resource
+    managed_resource = _materialize_overlay(
+        template=function.template,
+        materializer=function.materializers.base,
+        inputs=base_inputs,
+    )
 
     # TODO: Add owners, if ManagedResource.behaviors.add-owner
 
-    resource = None
-    if managed_resource:
-        computed_metadata = managed_resource.get("metadata", {})
+    resource = await _resource_crud(
+        api=api,
+        managed_resource=managed_resource,
+        resource_config=function.managed_resource,
+        on_create=function.materializers.on_create,
+        inputs=base_inputs,
+    )
+    if is_error(resource):
+        return resource
 
-        name = computed_metadata.get("name")
-        namespace = computed_metadata.get("namespace")
+    inputs = base_inputs | {
+        "resource": celpy.json_to_cel(resource) if resource else None
+    }
 
-        if function.managed_resource.crd:
-            api_version = function.managed_resource.crd.api_version
-            kind = function.managed_resource.crd.kind
-            plural = function.managed_resource.crd.plural
-            namespaced = function.managed_resource.crd.namespaced
-        else:
-            api_version = managed_resource.get("apiVersion")
-            kind = managed_resource.get("kind")
-            plural = None
-            namespaced = True if namespace else False
-
-        if not (api_version and kind and name):
-            logging.error("Missing critical managed resource fields!")
-            raise Exception("FAILURE")
-
-        resource_class = kr8s.objects.new_class(
-            version=api_version,
-            kind=kind,
-            plural=plural,
-            namespaced=namespaced,
-            asyncio=True,
-        )
-
-        try:
-            resource = await api.async_get(
-                resource_class,
-                name,
-                namespace=namespace,
-            )
-        except kr8s.NotFoundError:
-            resource = None
-        except kr8s.ServerError as err:
-            if err.response and err.response.status_code == 404:
-                resource = None
-            else:
-                logging.exception(
-                    f"ServerError loading {kind} resource {namespace}/{name}. ({type(err)}: {err})"
-                )
-                return Retry(
-                    message=f"Non-404 ServerError loading {kind} resource {namespace}/{name}. ({type(err)}: {err})",
-                    delay=30,
-                )
-        except Exception as err:
-            logging.exception(
-                f"Failure loading {kind} resource {namespace}/{name}. ({type(err)}: {err})"
-            )
-            return Retry(
-                message=f"Error loading {kind} resource {namespace}/{name}. ({type(err)}: {err})",
-                delay=30,
-            )
-
-        if not resource and function.managed_resource.behaviors.create:
-            if function.materializers.on_create:
-                on_create_overlay = json.loads(
-                    json.dumps(
-                        function.materializers.on_create.evaluate(
-                            {
-                                "inputs": converted_inputs,
-                                "template": celpy.json_to_cel(managed_resource),
-                            }
-                        )
-                    )
-                )
-                if on_create_overlay:
-                    for field_path, value in on_create_overlay.items():
-                        field_expr = jsonpath_ng.parse(field_path)
-                        field_expr.update_or_create(managed_resource, value)
-
-            logging.info(f"Creating {kind}.{api_version} resource {namespace}/{name}.")
-            new_object = resource_class(
-                api=api, resource=managed_resource, namespace=namespace
-            )
-            await new_object.create()
-            # TODO: Dynamic create delay?
-            return Retry(
-                message=f"Creating {kind}.{api_version} resource {namespace}/{name}.",
-                delay=30,
-            )
-
-        for resource_match in resource:
-            if not _validate_match(managed_resource, resource_match.raw):
-                logging.info(
-                    f"{kind}.{api_version}/{name} resource did not match spec, updating!"
-                )
-                if function.managed_resource.behaviors.update == "patch":
-                    # TODO: Should this only send fields with differences?
-                    await resource_match.patch(managed_resource)
-                    return Retry(
-                        message=f"Updating {kind}.{api_version} resource {namespace}/{name} to match template.",
-                        delay=5,
-                    )
-                elif function.managed_resource.behaviors.update == "recreate":
-                    await resource_match.delete()
-                    return Retry(
-                        message=f"Deleting {kind}.{api_version} resource {namespace}/{name} to recreate.",
-                        delay=5,
-                    )
-            else:
-                logging.info(
-                    f"{kind}.{api_version}/{name} resource matched spec, skipping!"
-                )
-
-    if function.outcome.tests:
-        outcome_test_results = json.loads(
-            json.dumps(
-                function.outcome.tests.evaluate(
-                    {
-                        "inputs": converted_inputs,
-                        "parent": trigger_resource,
-                        "resource": (
-                            celpy.json_to_cel(resource[0].raw) if resource else None
-                        ),
-                    }
-                )
-            )
-        )
-        outcome = _predicate_to_koreo_result(outcome_test_results)
-        if not is_ok(outcome):
-            return outcome
+    outcome_tests_outcome = _run_checks(checks=function.outcome.tests, inputs=inputs)
+    if not is_ok(outcome_tests_outcome):
+        return outcome_tests_outcome
 
     if not function.outcome.ok_value:
         return Ok(None)
 
     try:
-        resource = celpy.json_to_cel(resource[0].raw) if resource else None
-        ok_value = function.outcome.ok_value.evaluate(
-            {
-                "inputs": converted_inputs,
-                "parent": trigger_resource,
-                "resource": resource,
-            }
-        )
+        ok_value = function.outcome.ok_value.evaluate(inputs)
         return Ok(json.loads(json.dumps(ok_value)))
     except celpy.CELEvalError as err:
-        logging.exception(f"Failure computing OK value. {err.tree}")
-        return PermFail(f"Failed to compute OK Value. {err.token}")
+        msg = f"CEL Eval Error computing OK value. {err.tree}"
+        logging.exception(msg)
+        return PermFail(msg)
     except:
-        logging.exception("Failure computing OK value.")
-        return PermFail("Failed to compute OK Value.")
+        msg = "Failure computing OK value."
+        logging.exception(msg)
+        return PermFail(msg)
+
+
+def _run_checks(checks: celpy.Runner | None, inputs: dict[str, celtypes.Value]):
+    if not checks:
+        return Ok(None)
+
+    check_results = json.loads(json.dumps(checks.evaluate(inputs)))
+    return _predicate_to_koreo_result(check_results)
 
 
 def _predicate_to_koreo_result(results: list) -> Outcome:
@@ -235,6 +122,182 @@ def _predicate_to_koreo_result(results: list) -> Outcome:
         return Ok(None)
 
     return combine(outcomes=outcomes)
+
+
+def _materialize_overlay(
+    template: dict[str, Any] | None,
+    materializer: celpy.Runner | None,
+    inputs: dict[str, celtypes.Value],
+):
+    managed_resource = copy.deepcopy(template) if template else {}
+
+    if not materializer:
+        return managed_resource
+
+    overlay = json.loads(
+        json.dumps(
+            materializer.evaluate(
+                inputs
+                | {
+                    "template": celpy.json_to_cel(managed_resource),
+                }
+            )
+        )
+    )
+
+    if overlay:
+        for field_path, value in overlay.items():
+            field_expr = jsonpath_ng.parse(field_path)
+            field_expr.update_or_create(managed_resource, value)
+
+    return managed_resource
+
+
+class _Resource(NamedTuple):
+    api_version: str | None
+    kind: str | None
+    plural: str | None
+    namespaced: bool
+    name: str | None
+    namespace: str | None
+
+
+def _build_resource_config(
+    managed_resource: dict,
+    resource_config: ManagedResource,
+):
+    computed_metadata = managed_resource.get("metadata", {})
+
+    name = computed_metadata.get("name")
+    namespace = computed_metadata.get("namespace")
+
+    if resource_config.crd:
+        api_version = resource_config.crd.api_version
+        kind = resource_config.crd.kind
+        plural = resource_config.crd.plural
+        namespaced = resource_config.crd.namespaced
+    else:
+        api_version = managed_resource.get("apiVersion")
+        kind = managed_resource.get("kind")
+        plural = None
+        namespaced = True if namespace else False
+
+    return _Resource(
+        api_version=api_version,
+        kind=kind,
+        plural=plural,
+        namespaced=namespaced,
+        name=name,
+        namespace=namespace,
+    )
+
+
+async def _resource_crud(
+    api: kr8s.Api,
+    managed_resource: dict,
+    resource_config: ManagedResource,
+    on_create: celpy.Runner | None,
+    inputs: dict[str, celtypes.Value],
+):
+    if not managed_resource:
+        return None
+
+    resource_params = _build_resource_config(
+        managed_resource=managed_resource, resource_config=resource_config
+    )
+
+    if not (
+        resource_params.api_version and resource_params.kind and resource_params.name
+    ):
+        return PermFail(
+            f"kind ({resource_params.kind}), apiVersion ({resource_params.api_version}), and metadata.name ({resource_params.name}) are all required."
+        )
+
+    resource_class = kr8s.objects.new_class(
+        version=resource_params.api_version,
+        kind=resource_params.kind,
+        plural=resource_params.plural,
+        namespaced=resource_params.namespaced,
+        asyncio=True,
+    )
+
+    try:
+        resource_matches = await api.async_get(
+            resource_class,
+            resource_params.name,
+            namespace=resource_params.namespace,
+        )
+    except kr8s.NotFoundError:
+        resource_matches = None
+    except kr8s.ServerError as err:
+        if err.response and err.response.status_code == 404:
+            resource_matches = None
+        else:
+            msg = f"ServerError loading {resource_params.kind} resource {resource_params.namespace}/{resource_params.name}. ({type(err)}: {err})"
+            logging.exception(msg)
+            return Retry(message=msg, delay=30)
+    except Exception as err:
+        msg = f"Failure loading {resource_params.kind} resource {resource_params.namespace}/{resource_params.name}. ({type(err)}: {err})"
+        logging.exception(msg)
+        return Retry(message=msg, delay=30)
+
+    if not resource_matches:
+        resource = None
+    else:
+        if len(resource_matches) > 1:
+            return PermFail(
+                f"{resource_params.kind}.{resource_params.api_version}/{resource_params.name} resource matched multiple resources."
+            )
+        else:
+            resource = resource_matches[0]
+
+    if not resource and resource_config.behaviors.create:
+        managed_resource = _materialize_overlay(
+            template=managed_resource,
+            materializer=on_create,
+            inputs=inputs,
+        )
+
+        logging.info(
+            f"Creating {resource_params.kind}.{resource_params.api_version} resource {resource_params.namespace}/{resource_params.name}."
+        )
+        new_object = resource_class(
+            api=api, resource=managed_resource, namespace=resource_params.namespace
+        )
+        await new_object.create()
+        # TODO: Dynamic create delay?
+        return Retry(
+            message=f"Creating {resource_params.kind}.{resource_params.api_version} resource {resource_params.namespace}/{resource_params.name}.",
+            delay=30,
+        )
+
+    if not resource:
+        return None
+
+    if _validate_match(managed_resource, resource.raw):
+        logging.debug(
+            f"{resource_params.kind}.{resource_params.api_version}/{resource_params.name} resource matched spec, skipping update."
+        )
+        return resource.raw
+
+    logging.info(
+        f"{resource_params.kind}.{resource_params.api_version}/{resource_params.name} resource did not match spec, updating."
+    )
+    if resource_config.behaviors.update == "patch":
+        # TODO: Should this only send fields with differences?
+        await resource.patch(managed_resource)
+        return Retry(
+            message=f"Updating {resource_params.kind}.{resource_params.api_version} resource {resource_params.namespace}/{resource_params.name} to match template.",
+            delay=5,
+        )
+    elif resource_config.behaviors.update == "recreate":
+        await resource.delete()
+        return Retry(
+            message=f"Deleting {resource_params.kind}.{resource_params.api_version} resource {resource_params.namespace}/{resource_params.name} to recreate.",
+            delay=5,
+        )
+
+    return resource.raw
 
 
 def _validate_match(target, actual):
