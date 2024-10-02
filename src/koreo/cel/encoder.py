0febc2834f2eba0ab82c6a8fbff87445fe04b35b
Robert Kluin <robert.kluin@realkinetic.com>
2024-10-01 20:02:01 -0600

    Add CEL encoder tests

--- src/koreo/cel/encoder.py
+++ src/koreo/cel/encoder.py
@@ -11,11 +11,13 @@ def encode_cel(value):
         ) }}}"
 
     if isinstance(value, list):
+        return f"[{ ",".join(encode_cel(item) for item in value) }]"
 
-        return f"[{ ",".join(encode_cel(list_value) for list_value in value) }]"
+    if isinstance(value, bool):
+        return "true" if value else "false"
 
     if _encode_plain(value):
-        return value
+        return f"{value}"
 
     if not value:
         return '""'
@@ -40,6 +42,9 @@ def _encode_template_dict(base: str, template_spec: dict):
 
         field_name = safe_field
         if base:
+            # TODO: QUESTION: I wonder if this should instead just check for
+            # '.'?  Maybe we should only allow json-path from the "root" level
+            # and not from nested fields?
             if base.endswith("labels") or base.endswith("annotations"):
                 field_name = f"{base}['{safe_field}']"
                 print(field_name)
